cdbf9c5fe9edaceb3c3f514877bcf392521189d9
Marvin Augustin <maugu028@fiu.edu>
2022-06-04 21:32:58 -0400

    Initial Commit

---
+++ SelectionSort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SelectionSort
+//
+//  Created by Marvin Augustin on 6/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
